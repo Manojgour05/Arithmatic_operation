7258c9fba1827e33cd889224a9731f53b718ec99
Manojgour05 <manojgour6811@gmail.com>
2020-10-01 22:12:07 +0530

    Refactore: Result stored in dictionary

--- arithmatic_Op.sh
+++ arithmatic_Op.sh
@@ -5,7 +5,7 @@ echo "Read 3 numbers"
 read a
 read b
 read c
-echo " $a $b $c"
+#echo " $a $b $c"
 
 #compute a+b*c
  op1=$(($a+$b*$c))
@@ -15,12 +15,21 @@ echo " $a $b $c"
 #compute  a*b+c
 op2=$(($a*$b+$c))
 
-#	echo $op2
+       #echo $op2
 
 # Compute c+a/b
 op3=$((($c+$a)/$b))
-#	echo $op3
+       #echo $op3
 
 #Compute  a%b+c
 op4=$((($a%$b)+$c))
-	echo $op4
+	#echo $op4
+
+# Declare dictonary  to store all results
+
+declare -A operation
+	operation[op1]="$op1"
+	operation[op2]="$op2"
+	operation[op3]="$op3"
+	operation[op4]="$op4"
+	echo ${operation[@]}
