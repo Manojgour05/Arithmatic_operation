9c1a94825a84a8dccc788a59caea0edb7215de1c
Manojgour05 <manojgour6811@gmail.com>
2020-10-01 23:18:26 +0530

    Refactore: Sorted in descending order

--- arithmatic_Op.sh
+++ arithmatic_Op.sh
@@ -8,21 +8,19 @@ read c
 #echo " $a $b $c"
 
 #compute a+b*c
- op1=$(($a+$b*$c))
-
+	 op1=$(($a+$b*$c))
 	#echo $op1
 
 #compute  a*b+c
-op2=$(($a*$b+$c))
-
-       #echo $op2
+	op2=$(($a*$b+$c))
+        #echo $op2
 
 # Compute c+a/b
-op3=$((($c+$a)/$b))
-       #echo $op3
+	op3=$((($c+$a)/$b))
+        #echo $op3
 
 #Compute  a%b+c
-op4=$((($a%$b)+$c))
+	op4=$((($a%$b)+$c))
 	#echo $op4
 
 # Declare dictonary  to store all results
@@ -32,13 +30,29 @@ declare -A operation
 	operation[op2]="$op2"
 	operation[op3]="$op3"
 	operation[op4]="$op4"
-	echo "results in dictionary :${operation[@]}"
+	#echo "results in dictionary :${operation[@]}"
 # Store in array
 arr=()
 inx=0
 for i in  ${operation[@]}
 do
 	arr[inx]=$i
-     inx=$(($inx+1))
+        inx=$(($inx+1))
 done
- echo "Array elements are: ${arr[@]}"
+	echo "Array elements are: ${arr[@]}"
+
+
+# Computation Result in the Descending Order
+for (( i=0; i<${#arr[@]}; i++ ))
+do
+	for(( j=i+1; j<${#arr[@]}; j++ ))
+	do
+		if [[ arr[$i]<arr[$j] ]]
+		then
+			temp=${arr[$i]}
+			arr[$i]=${arr[$j]}
+			arr[$j]=$temp
+		fi
+	done
+done
+echo "Elements are sorted in descendingorder : ${arr[@]}"
